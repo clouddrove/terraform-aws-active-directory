ef241c434cdde4bf4663de1f9784adff38d490cb
Manish <mkshah987@gmail.com>
2025-03-10 15:42:42 +0530

    fix: removed unnessary variables

--- examples/AD-Connector/variable.tf
+++ examples/AD-Connector/variable.tf
@@ -1,26 +1,3 @@
-variable "customer_dns_ips" {
-  type        = list(string)
-  description = "(Required) The DNS IP addresses of the domain to connect to."
-  default     = []
-}
-
-variable "enabled" {
-  type        = bool
-  default     = true
-  description = "Flag to control the module creation."
-}
-variable "customer_username" {
-  type        = string
-  description = "(Required) The username corresponding to the password provided."
-  default     = ""
-}
-
-variable "directory_type" {
-  default     = "ADConnector"
-  type        = string
-  description = "The directory type (SimpleAD, ADConnector or MicrosoftAD are accepted values)."
-}
-
 variable "directory_size" {
   description = "The size of the directory (Small or Large are accepted values). Small by default."
   type        = string
@@ -45,17 +22,4 @@ variable "ip_rules" {
   ]
 }
 
-variable "vpc_settings" {
-  type        = map(string)
-  default     = {}
-  description = "(Required for SimpleAD and MicrosoftAD) VPC related information about the directory. Fields documented below."
-
-}
-
-variable "ad_password" {
-  default     = ""
-  type        = string
-  description = "The password for the directory administrator or connector user."
-  sensitive   = true
-}
 
